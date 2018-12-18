bb378241b9a1420f0e9290655c958d9f3e5de3c9
Maria T <36854099+MariaT7@users.noreply.github.com>
2018-12-18 11:17:24 -0500

    Initial commit

---
+++ Better21Maria/Better21Form.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Better21Maria
+{
+    partial class frmBetter21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.lblPTitle = new System.Windows.Forms.Label();
+            this.lblDTitle = new System.Windows.Forms.Label();
+            this.lblPlayerValue = new System.Windows.Forms.Label();
+            this.lblDealerValue = new System.Windows.Forms.Label();
+            this.picCardp1 = new System.Windows.Forms.PictureBox();
+            this.picCardp3 = new System.Windows.Forms.PictureBox();
+            this.picCardp2 = new System.Windows.Forms.PictureBox();
+            this.picCardd3 = new System.Windows.Forms.PictureBox();
+            this.picCardd1 = new System.Windows.Forms.PictureBox();
+            this.picCardd2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardp1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardp3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardp2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardd3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardd1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardd2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Monotxt_IV50", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(491, 237);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 37);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(968, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnStay
+            // 
+            this.btnStay.Font = new System.Drawing.Font("OCR A Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStay.Location = new System.Drawing.Point(675, 360);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(90, 49);
+            this.btnStay.TabIndex = 2;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("OCR A Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(319, 360);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(90, 49);
+            this.btnHit.TabIndex = 3;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            // 
+            // lblPTitle
+            // 
+            this.lblPTitle.AutoSize = true;
+            this.lblPTitle.BackColor = System.Drawing.Color.ForestGreen;
+            this.lblPTitle.Font = new System.Drawing.Font("Monotxt_IV50", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPTitle.Location = new System.Drawing.Point(312, 96);
+            this.lblPTitle.Name = "lblPTitle";
+            this.lblPTitle.Size = new System.Drawing.Size(108, 26);
+            this.lblPTitle.TabIndex = 4;
+            this.lblPTitle.Text = "Player";
+            // 
+            // lblDTitle
+            // 
+            this.lblDTitle.AutoSize = true;
+            this.lblDTitle.BackColor = System.Drawing.Color.ForestGreen;
+            this.lblDTitle.Font = new System.Drawing.Font("Monotxt_IV50", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDTitle.Location = new System.Drawing.Point(665, 96);
+            this.lblDTitle.Name = "lblDTitle";
+            this.lblDTitle.Size = new System.Drawing.Size(108, 26);
+            this.lblDTitle.TabIndex = 5;
+            this.lblDTitle.Text = "Dealer";
+            // 
+            // lblPlayerValue
+            // 
+            this.lblPlayerValue.AutoSize = true;
+            this.lblPlayerValue.Location = new System.Drawing.Point(332, 436);
+            this.lblPlayerValue.Name = "lblPlayerValue";
+            this.lblPlayerValue.Size = new System.Drawing.Size(66, 13);
+            this.lblPlayerValue.TabIndex = 6;
+            this.lblPlayerValue.Text = "Player Value";
+            // 
+            // lblDealerValue
+            // 
+            this.lblDealerValue.AutoSize = true;
+            this.lblDealerValue.Location = new System.Drawing.Point(685, 436);
+            this.lblDealerValue.Name = "lblDealerValue";
+            this.lblDealerValue.Size = new System.Drawing.Size(68, 13);
+            this.lblDealerValue.TabIndex = 7;
+            this.lblDealerValue.Text = "Dealer Value";
+            // 
+            // picCardp1
+            // 
+            this.picCardp1.Location = new System.Drawing.Point(239, 128);
+            this.picCardp1.Name = "picCardp1";
+            this.picCardp1.Size = new System.Drawing.Size(87, 101);
+            this.picCardp1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCardp1.TabIndex = 8;
+            this.picCardp1.TabStop = false;
+            // 
+            // picCardp3
+            // 
+            this.picCardp3.Location = new System.Drawing.Point(319, 237);
+            this.picCardp3.Name = "picCardp3";
+            this.picCardp3.Size = new System.Drawing.Size(87, 101);
+            this.picCardp3.TabIndex = 9;
+            this.picCardp3.TabStop = false;
+            // 
+            // picCardp2
+            // 
+            this.picCardp2.Location = new System.Drawing.Point(407, 128);
+            this.picCardp2.Name = "picCardp2";
+            this.picCardp2.Size = new System.Drawing.Size(87, 101);
+            this.picCardp2.TabIndex = 10;
+            this.picCardp2.TabStop = false;
+            // 
+            // picCardd3
+            // 
+            this.picCardd3.Location = new System.Drawing.Point(752, 237);
+            this.picCardd3.Name = "picCardd3";
+            this.picCardd3.Size = new System.Drawing.Size(87, 103);
+            this.picCardd3.TabIndex = 13;
+            this.picCardd3.TabStop = false;
+            // 
+            // picCardd1
+            // 
+            this.picCardd1.Location = new System.Drawing.Point(675, 128);
+            this.picCardd1.Name = "picCardd1";
+            this.picCardd1.Size = new System.Drawing.Size(87, 101);
+            this.picCardd1.TabIndex = 12;
+            this.picCardd1.TabStop = false;
+            // 
+            // picCardd2
+            // 
+            this.picCardd2.Location = new System.Drawing.Point(598, 237);
+            this.picCardd2.Name = "picCardd2";
+            this.picCardd2.Size = new System.Drawing.Size(87, 103);
+            this.picCardd2.TabIndex = 11;
+            this.picCardd2.TabStop = false;
+            // 
+            // frmBetter21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Better21Maria.Properties.Resources.live_casino9;
+            this.ClientSize = new System.Drawing.Size(968, 521);
+            this.Controls.Add(this.picCardd3);
+            this.Controls.Add(this.picCardd1);
+            this.Controls.Add(this.picCardd2);
+            this.Controls.Add(this.picCardp2);
+            this.Controls.Add(this.picCardp3);
+            this.Controls.Add(this.picCardp1);
+            this.Controls.Add(this.lblDealerValue);
+            this.Controls.Add(this.lblPlayerValue);
+            this.Controls.Add(this.lblDTitle);
+            this.Controls.Add(this.lblPTitle);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnStay);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmBetter21";
+            this.Text = "Better 21";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardp1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardp3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardp2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardd3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardd1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCardd2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Label lblPTitle;
+        private System.Windows.Forms.Label lblDTitle;
+        private System.Windows.Forms.Label lblPlayerValue;
+        private System.Windows.Forms.Label lblDealerValue;
+        private System.Windows.Forms.PictureBox picCardp1;
+        private System.Windows.Forms.PictureBox picCardp3;
+        private System.Windows.Forms.PictureBox picCardp2;
+        private System.Windows.Forms.PictureBox picCardd3;
+        private System.Windows.Forms.PictureBox picCardd1;
+        private System.Windows.Forms.PictureBox picCardd2;
+    }
+}
+
---
+++ Better21Maria/Better21Form.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Better21Maria
+{
+
+    public partial class frmBetter21 : Form
+    {
+        //declare global variables
+        int playerTotal = 0, dealerTotal;
+        List<Image> listCardImages = new List<Image>();
+        Random randomNumGen;
+
+
+        public frmBetter21()
+        {
+            InitializeComponent();
+            randomNumGen = new Random();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //deal initial cards
+            playerTotal = DealCard(ref playerTotal, listCardImages);
+            playerTotal = DealCard(ref playerTotal, listCardImages);
+
+            dealerTotal = DealCard(ref dealerTotal, listCardImages);
+            dealerTotal = DealCard(ref dealerTotal, listCardImages);
+
+            for (int index = 0; index > listCardImages.Count(); index++)
+            {
+                listCardImages.RemoveAt(index);
+            }
+
+            //populate deck
+            listCardImages.Add(Properties.Resources.AC);
+            listCardImages.Add(Properties.Resources.AD);
+            listCardImages.Add(Properties.Resources.AH);
+            listCardImages.Add(Properties.Resources.AS);
+            listCardImages.Add(Properties.Resources._2C);
+            listCardImages.Add(Properties.Resources._2D);
+            listCardImages.Add(Properties.Resources._2H);
+            listCardImages.Add(Properties.Resources._2S);
+            listCardImages.Add(Properties.Resources._3C);
+            listCardImages.Add(Properties.Resources._3D);
+            listCardImages.Add(Properties.Resources._3H);
+            listCardImages.Add(Properties.Resources._3S);
+            listCardImages.Add(Properties.Resources._4C);
+            listCardImages.Add(Properties.Resources._4D);
+            listCardImages.Add(Properties.Resources._4H);
+            listCardImages.Add(Properties.Resources._4S);
+            listCardImages.Add(Properties.Resources._5C);
+            listCardImages.Add(Properties.Resources._5D);
+            listCardImages.Add(Properties.Resources._5H);
+            listCardImages.Add(Properties.Resources._5S);
+            listCardImages.Add(Properties.Resources._6C);
+            listCardImages.Add(Properties.Resources._6D);
+            listCardImages.Add(Properties.Resources._6H);
+            listCardImages.Add(Properties.Resources._6S);
+            listCardImages.Add(Properties.Resources._7C);
+            listCardImages.Add(Properties.Resources._7D);
+            listCardImages.Add(Properties.Resources._7H);
+            listCardImages.Add(Properties.Resources._7S);
+            listCardImages.Add(Properties.Resources._8C);
+            listCardImages.Add(Properties.Resources._8D);
+            listCardImages.Add(Properties.Resources._8H);
+            listCardImages.Add(Properties.Resources._8S);
+            listCardImages.Add(Properties.Resources._9C);
+            listCardImages.Add(Properties.Resources._9D);
+            listCardImages.Add(Properties.Resources._9H);
+            listCardImages.Add(Properties.Resources._9S);
+            listCardImages.Add(Properties.Resources._10C);
+            listCardImages.Add(Properties.Resources._10D);
+            listCardImages.Add(Properties.Resources._10H);
+            listCardImages.Add(Properties.Resources._10S);
+            listCardImages.Add(Properties.Resources.JC);
+            listCardImages.Add(Properties.Resources.JD);
+            listCardImages.Add(Properties.Resources.JH);
+            listCardImages.Add(Properties.Resources.JS);
+            listCardImages.Add(Properties.Resources.KC);
+            listCardImages.Add(Properties.Resources.KD);
+            listCardImages.Add(Properties.Resources.KH);
+            listCardImages.Add(Properties.Resources.KS);
+            listCardImages.Add(Properties.Resources.QC);
+            listCardImages.Add(Properties.Resources.QD);
+            listCardImages.Add(Properties.Resources.QH);
+            listCardImages.Add(Properties.Resources.QS);
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //creates a new form 
+            using (var form1 = new frmBetter21())
+            {
+                //shows new form and closes old
+                Visible = false;
+                form1.ShowDialog();
+                Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private int DealCard(ref int total, List<Image>listCardImages)
+        {
+            //Declare local variables
+            int index, currentTotal = 0;
+
+            //generate a random number between 0 and list length
+            index = randomNumGen.Next(1, listCardImages.Count() + 1);
+            picCardp1.Image = listCardImages[index];
+            picCardp2.Image = listCardImages[index];
+
+            return currentTotal;
+            
+        }
+    }
+}
